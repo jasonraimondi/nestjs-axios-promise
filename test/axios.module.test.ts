07063bebe027d1e26cac971daebaccbb33c38005
Keith Wade <keith@keithwade.com>
2024-11-01 17:58:14 -0400

    Enhance AxiosModule to allow registering Axios interceptors at module construction

--- test/axios.module.test.ts
+++ test/axios.module.test.ts
@@ -3,17 +3,111 @@ import { Test } from "@nestjs/testing";
 import { AxiosModule, AxiosService } from "../src";
 
 describe(AxiosModule, () => {
-  let axiosService: AxiosService;
-
-  beforeEach(async () => {
+  it("axios module export service", async () => {
     const moduleRef = await Test.createTestingModule({
       imports: [AxiosModule.register({})],
     }).compile();
 
-    axiosService = moduleRef.get<AxiosService>(AxiosService);
+    const axiosService = moduleRef.get<AxiosService>(AxiosService);
+
+    expect(axiosService).toBeDefined();
   });
 
-  it("axios module export service", () => {
+  it("should allow registering asynchronously", async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [
+        AxiosModule.registerAsync({
+          useFactory: () => {
+            return {
+              config: {},
+            };
+          },
+        }),
+      ],
+    }).compile();
+
+    const axiosService = moduleRef.get<AxiosService>(AxiosService);
+
     expect(axiosService).toBeDefined();
+  });
+
+  it("should register interceptors", async () => {
+    const responseOnFulfilled = jest.fn();
+    const requestOnFulfilled = jest.fn();
+
+    const moduleRef = await Test.createTestingModule({
+      imports: [
+        AxiosModule.register({
+          interceptors: {
+            response: {
+              onFulfilled: (response) => {
+                responseOnFulfilled("response");
+
+                return response;
+              },
+            },
+
+            request: {
+              onFulfilled: (requestConfig) => {
+                requestOnFulfilled("request");
+
+                return requestConfig;
+              },
+            },
+          },
+        }),
+      ],
+    }).compile();
+
+    const axiosService = moduleRef.get<AxiosService>(AxiosService);
+
+    await axiosService.get("https://github.com");
+
+    expect(responseOnFulfilled).toHaveBeenCalledTimes(1);
+    expect(responseOnFulfilled).toHaveBeenCalledWith("response");
+
+    expect(requestOnFulfilled).toHaveBeenCalledTimes(1);
+    expect(requestOnFulfilled).toHaveBeenCalledWith("request");
+  });
+
+  it("should async register interceptors", async () => {
+    const responseOnFulfilled = jest.fn();
+    const requestOnFulfilled = jest.fn();
+
+    const moduleRef = await Test.createTestingModule({
+      imports: [
+        AxiosModule.registerAsync({
+          useFactory: () => ({
+            interceptors: {
+              response: {
+                onFulfilled: (response) => {
+                  responseOnFulfilled("response");
+
+                  return response;
+                },
+              },
+
+              request: {
+                onFulfilled: (requestConfig) => {
+                  requestOnFulfilled("request");
+
+                  return requestConfig;
+                },
+              },
+            },
+          }),
+        }),
+      ],
+    }).compile();
+
+    const axiosService = moduleRef.get<AxiosService>(AxiosService);
+
+    await axiosService.get("https://github.com");
+
+    expect(responseOnFulfilled).toHaveBeenCalledTimes(1);
+    expect(responseOnFulfilled).toHaveBeenCalledWith("response");
+
+    expect(requestOnFulfilled).toHaveBeenCalledTimes(1);
+    expect(requestOnFulfilled).toHaveBeenCalledWith("request");
   });
 });
